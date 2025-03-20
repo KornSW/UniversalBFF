b8fbef07f67cb05f120f460aa0211c8f0df4f6a3
T. Korn <tobias.korn@towerswatson.com>
2025-03-20 20:45:49 +0100

    OOB Modules instead of Demo

---
+++ dotnet/src/UniversalBFF.OobModules.Edmx/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataModel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataModel")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("827bdd27-c32b-4bb4-b6e4-6914c95cae2d")]
+
+[assembly: AssemblyVersion("999.0.0")]
+[assembly: AssemblyInformationalVersion("999.0.0-localbuild")]
---
+++ dotnet/src/UniversalBFF.OobModules.UserManagement/Backend/DemoBackendService.cs
@@ -0,0 +1,16 @@
+﻿using Logging.SmartStandards;
+using System;
+
+namespace UniversalBFF.Demo {
+
+  public class DemoBackendService : IDemoBackendService {
+
+    public void Test() {
+
+      DevLogger.LogInformation(0, "DemoBackendService says Test!");
+
+    }
+
+  }
+
+}
