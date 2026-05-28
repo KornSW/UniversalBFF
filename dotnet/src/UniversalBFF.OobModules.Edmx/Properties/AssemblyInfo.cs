dfe454e0073f2f5809298a9de9ef9dc81c729442
CI Build <ci@ushell.org>
2026-05-28 10:57:12 +0000

    Some Cleanup again -> VERSIONING [skip ci]

--- dotnet/src/UniversalBFF.OobModules.Edmx/Properties/AssemblyInfo.cs
+++ dotnet/src/UniversalBFF.OobModules.Edmx/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("827bdd27-c32b-4bb4-b6e4-6914c95cae2d")]
 
-[assembly: AssemblyVersion("0.3.3")]
-[assembly: AssemblyInformationalVersion("0.3.3")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
